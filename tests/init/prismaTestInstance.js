5d03f5bb146bd6e281c805a4a0c237e5616179c1
steph4104 <stephanie4104@gmail.com>
2019-03-19 09:35:37 -0400

    remove default

--- tests/init/prismaTestInstance.js
+++ tests/init/prismaTestInstance.js
@@ -1,5 +1,4 @@
 const { Prisma } = require("prisma-binding");
-const { getDefaults } = require("../../src/resolvers/helper/default_setup");
 
 function setPrisma(){
   
@@ -9,24 +8,11 @@ function setPrisma(){
       });
 }
 
-async function getContext(){
-  var ctx = {};
-  ctx.prisma = await new Prisma({
-    typeDefs: "src/generated/prisma.graphql",
-    endpoint: "http://localhost:4466",
-    })
-
-  ctx.defaults = await getDefaults(ctx);
-  return ctx;
- 
-};
-
 async function cleanUp(context){
     await context.prisma.mutation.deleteManyNotifications();
   }
 
 module.exports = {
-  getContext,
   cleanUp,
   setPrisma
 };
