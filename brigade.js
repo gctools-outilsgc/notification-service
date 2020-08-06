7dd53ca7232f3baa056635754679c0b33abfe5b0
Ilia <phanoix@gmail.com>
2020-08-06 15:44:53 -0400

    Create brigade.js

---
+++ brigade.js
@@ -0,0 +1,25 @@
+const { events, Job } = require("brigadier");
+
+events.on("image_push", function(e, project) {
+  var docker = JSON.parse(e.payload)
+
+  if (docker.push_data.tag != "latest") {
+    console.log(`ignoring non-master build for branch ${docker.push_data.tag}`)
+    return
+  }
+  
+  var update = new Job("update", "python:3")
+  update.tasks = [
+    "pip install kubernetes",
+    "python /src/.brigade/test.py"
+  ]
+  
+  var notify = new Job("notify", "alpine:3.4")
+  notify.env.CHATKEY = project.secrets.chatKey
+  notify.tasks = [
+    "apk update && apk add curl",
+    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"naas test deployment updated.\"}' https://message.gccollab.ca/hooks/$CHATKEY"      //test rocket chat notification
+  ]
+  
+  update.run().then(() => { notify.run() })
+})
