1102dc9118d2ee6954051298c7fa1af89f8d85c7
Ilia <phanoix@gmail.com>
2020-08-06 15:38:01 -0400

    Create update.py

---
+++ .brigade/update.py
@@ -0,0 +1,44 @@
+
+import json
+import requests
+
+from kubernetes import client, config
+
+def get_latest_sha(repo):
+    latest = requests.get("https://hub.docker.com/v2/repositories/" + repo + "/tags/latest")
+    data = json.loads(latest.text)
+    sha = data["images"][0]["digest"]
+    return sha
+
+def k8s_get_deploy(name):
+    config.load_incluster_config()
+
+    v1 = client.AppsV1Api()
+    deploys = v1.list_namespaced_deployment(watch=False, namespace="naas")
+    for i in deploys.items:
+        if i.metadata.name == name:
+            print("=== %s\t%s\t%s\t%s" %
+                (i.spec.replicas, i.metadata.namespace, i.metadata.name, i.spec.template.spec.containers[0].image))
+            return i
+
+    return False
+
+def k8s_patch_deploy(deploy):
+    v1 = client.AppsV1Api()
+
+    api_response = v1.patch_namespaced_deployment( name=deploy.metadata.name, namespace="directory", body=deploy)
+    print("Deployment updated. status='%s'" % str(api_response.status))
+
+if __name__ == "__main__":
+    repo = "digitalcollab/notifications-apollo"
+    digest=get_latest_sha(repo)
+
+    config.load_incluster_config()
+    deploy = k8s_get_deploy("notification-apollo")
+
+    print("\n")
+    print("%s\t%s\t%s\t%s" %
+        (deploy.spec.replicas, deploy.metadata.namespace, deploy.metadata.name, deploy.spec.template.spec.containers[0].image))
+
+    deploy.spec.template.spec.containers[0].image = repo+"@"+digest
+    k8s_patch_deploy(deploy)
